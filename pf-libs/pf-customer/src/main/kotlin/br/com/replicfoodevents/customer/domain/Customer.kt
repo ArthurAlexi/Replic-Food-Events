bf6c2dcdc9a9a5b0861d0d1f5b80ef4bfc4a343e
Arthur Alexi <90854173+ArthurAlexi@users.noreply.github.com>
2024-11-07 17:56:17 -0300

    feat: implementation customer domain

---
+++ pf-libs/pf-customer/src/main/kotlin/br/com/replicfoodevents/customer/domain/Customer.kt
@@ -0,0 +1,50 @@
+package br.com.replicfoodevents.customer.domain
+
+import br.com.replicfoodevents.common.domain.api.model.Money
+import br.com.replicfoodevents.common.domain.api.model.PersonName
+import br.com.replicfoodevents.customer.domain.api.CreateCustomerCommand
+import br.com.replicfoodevents.customer.domain.api.CreateCustomerOrderCommand
+import br.com.replicfoodevents.customer.domain.api.CustomerCreatedEvent
+import br.com.replicfoodevents.customer.domain.api.model.CustomerId
+import org.apache.commons.lang3.builder.EqualsBuilder
+import org.apache.commons.lang3.builder.HashCodeBuilder
+import org.apache.commons.lang3.builder.ToStringBuilder
+import org.axonframework.commandhandling.CommandHandler
+import org.axonframework.eventsourcing.EventSourcingHandler
+import org.axonframework.modelling.command.AggregateIdentifier
+import org.axonframework.modelling.command.AggregateLifecycle
+import org.axonframework.spring.stereotype.Aggregate
+
+@Aggregate
+internal class Customer {
+    @AggregateIdentifier
+    private lateinit var id: CustomerId
+    private lateinit var person: PersonName
+    private lateinit var orderList: Money
+
+    constructor()
+    @CommandHandler
+    constructor(command: CreateCustomerCommand ) {
+        AggregateLifecycle.apply(CustomerCreatedEvent(
+            person = command.person,
+            orderLimit = command.orderLimit,
+            aggregateIdentifier = command.targetAggregateIdentifier,
+            auditEntry = command.auditEntry))
+    }
+    @EventSourcingHandler
+    fun on(event: CustomerCreatedEvent) {
+        this.id = event.aggregateIdentifier
+        this.person = event.person
+        this.orderList = event.orderLimit
+    }
+    @CommandHandler
+    fun han(command: CreateCustomerOrderCommand){
+        if( !command.orderTotal.isGreaterThanOrEqual(orderList)){
+            AggregateLifecycle.createNew(CustomerOrder::class.java) { CustomerOrder(command) }
+        }
+    }
+
+    override fun toString(): String = ToStringBuilder.reflectionToString(this)
+    override fun equals(other: Any?): Boolean = EqualsBuilder.reflectionEquals(this, other)
+    override fun hashCode(): Int = HashCodeBuilder.reflectionHashCode(this)
+}
---
+++ pf-libs/pf-customer/src/main/kotlin/br/com/replicfoodevents/customer/domain/CustomerOrder.kt
@@ -0,0 +1,63 @@
+package br.com.replicfoodevents.customer.domain
+
+import br.com.replicfoodevents.common.domain.api.model.Money
+import br.com.replicfoodevents.customer.domain.api.CreateCustomerOrderCommand
+import br.com.replicfoodevents.customer.domain.api.CustomerOrderCreatedEvent
+import br.com.replicfoodevents.customer.domain.api.CustomerOrderDeliveredEvent
+import br.com.replicfoodevents.customer.domain.api.MarkCustomerOrderAsDeliveredCommand
+import br.com.replicfoodevents.customer.domain.api.model.CustomerId
+import br.com.replicfoodevents.customer.domain.api.model.CustomerOrderId
+import br.com.replicfoodevents.customer.domain.api.model.CustomerOrderState
+import org.apache.commons.lang3.builder.EqualsBuilder
+import org.apache.commons.lang3.builder.HashCodeBuilder
+import org.apache.commons.lang3.builder.ToStringBuilder
+import org.axonframework.commandhandling.CommandHandler
+import org.axonframework.eventsourcing.EventSourcingHandler
+import org.axonframework.modelling.command.AggregateIdentifier
+import org.axonframework.modelling.command.AggregateLifecycle
+import org.axonframework.spring.stereotype.Aggregate
+
+@Aggregate
+internal class CustomerOrder {
+
+    @AggregateIdentifier
+    private lateinit var id: CustomerOrderId
+    private lateinit var customerId: CustomerId
+    private lateinit var orderTotal: Money
+    private lateinit var state: CustomerOrderState
+
+    constructor()
+
+    constructor(command: CreateCustomerOrderCommand){
+        AggregateLifecycle.apply(CustomerOrderCreatedEvent(
+            aggregateIdentifier = command.targetAggregateIdentifier,
+            customerOrderId = command.customerOrderId,
+            orderTotal = command.orderTotal,
+            auditEntry = command.auditEntry
+        ))
+    }
+    @EventSourcingHandler
+    fun on(event: CustomerOrderCreatedEvent){
+        this.id = event.customerOrderId
+        this.customerId = event.aggregateIdentifier
+        this.orderTotal = event.orderTotal
+        this.state = CustomerOrderState.CREATED
+    }
+    @CommandHandler
+    fun han(command: MarkCustomerOrderAsDeliveredCommand){
+        if(CustomerOrderState.CREATED == this.state) {
+            AggregateLifecycle.apply(CustomerOrderDeliveredEvent(
+                command.targetAggregateIdentifier,
+                command.auditEntry
+            ))
+        }
+    }
+    @EventSourcingHandler
+    fun on(event: CustomerOrderDeliveredEvent){
+        this.state = CustomerOrderState.DELIVERED
+    }
+
+    override fun toString(): String = ToStringBuilder.reflectionToString(this)
+    override fun equals(other: Any?): Boolean = EqualsBuilder.reflectionEquals(this, other)
+    override fun hashCode(): Int = HashCodeBuilder.reflectionHashCode(this)
+}
